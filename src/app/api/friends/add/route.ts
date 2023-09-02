a5a9e0d9f24849bec2f2f23b759b1d47e1e6bfd0
nishant-spendflo <112845053+nishant-spendflo@users.noreply.github.com>
2023-09-02 16:48:10 +0545

    sending friend request

---
+++ src/app/api/friends/add/route.ts
@@ -0,0 +1,74 @@
+import { fetchRedis } from "@/helpers/redis";
+import { authOptions } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { addFriendValidator } from "@/lib/validations/add-friend";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+import { z } from "zod";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+
+    const { email: emailToAdd } = addFriendValidator.parse(body);
+
+    const RESTResponse = await fetch(
+      `${process.env.UPSTASH_REDIS_REST_URL}/get/user:email:${emailToAdd}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.UPSTASH_REDIS_REST_TOKEN}`,
+        },
+        cache: "no-store",
+      }
+    );
+
+    const data = (await RESTResponse.json()) as { result: string | null };
+    const idToAdd = data.result;
+    if (!idToAdd) {
+      return new NextResponse("This person does not exist.", { status: 400 });
+    }
+
+    const session = await getServerSession(authOptions);
+    if (!session) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+    if (idToAdd === session.user.id) {
+      return new NextResponse("Your cannot add yourself as a friend.", {
+        status: 400,
+      });
+    }
+
+    // check if user is already added
+    const isAlreadyAdded = (await fetchRedis(
+      "sismember",
+      `user:${idToAdd}:incoming_friend_request`,
+      session.user.id
+    )) as 0 | 1;
+
+    if (isAlreadyAdded) {
+      return new NextResponse("Already added this user.", { status: 400 });
+    }
+
+    // check if user is already added
+    const isAlreadyFriends = (await fetchRedis(
+      "sismember",
+      `user:${session.user.id}:friends`,
+      idToAdd
+    )) as 0 | 1;
+
+    if (isAlreadyFriends) {
+      return new NextResponse("Already friends with this user.", {
+        status: 400,
+      });
+    }
+    // valid request, send friend request
+
+    db.sadd(`user:${idToAdd}:incoming_friend_request`, session.user.id);
+    return new NextResponse("OK");
+  } catch (error: any) {
+    if (error instanceof z.ZodError) {
+      return new NextResponse("Invalid request payload", { status: 422 });
+    }
+    return new NextResponse("Invalid request", { status: 400 });
+  }
+}
