d41776c0994fc68433b140d3f1440eb18513462a
nishant-spendflo <112845053+nishant-spendflo@users.noreply.github.com>
2023-09-02 20:40:52 +0545

    added profile (bottom) of sidebar

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,5 +1,6 @@
 import { Icon, Icons } from "@/components/Icons";
 import { getServerSession } from "next-auth";
+import Image from "next/image";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 import { ReactNode } from "react";
@@ -62,6 +63,26 @@ const DashboardLayout = async ({ children }: DashboardLayoutProps) => {
                   );
                 })}
               </ul>
+            </li>
+            <li className="-mx-6 mt-auto flex items-center">
+              <div className="flex flex-1 items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-gray-900">
+                <div className="relative h-8 w-8 bg-gray-50">
+                  <Image
+                    fill
+                    referrerPolicy="no-referrer"
+                    className="rounded-full"
+                    src={session?.user?.image || ""}
+                    alt="Your profile picture"
+                  />
+                </div>
+                <span className="sr-only">Your profile</span>
+                <div className="flex flex-col">
+                  <span aria-hidden="true">{session?.user?.name}</span>
+                  <span className="text-xs text-zinc-400" aria-hidden="true">
+                    {session?.user?.email}
+                  </span>
+                </div>
+              </div>
             </li>
           </ul>
         </nav>
