5b8ae9d34dbeaf358f8217c797acb36d71492873
nishant-spendflo <112845053+nishant-spendflo@users.noreply.github.com>
2023-09-07 20:15:39 +0545

    added same layout in  mobile from dashboard layout

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -6,6 +6,7 @@ import SignOutButton from "@/components/SignOutButton";
 import { getFriendsByUserId } from "@/helpers/get-friends-by-user-id";
 import { fetchRedis } from "@/helpers/redis";
 import { authOptions } from "@/lib/auth";
+import { SidebarOption } from "@/types/typings";
 import { getServerSession } from "next-auth";
 import Image from "next/image";
 import Link from "next/link";
@@ -14,13 +15,6 @@ import { ReactNode } from "react";
 
 interface DashboardLayoutProps {
   children: ReactNode;
-}
-
-interface SidebarOption {
-  id: number;
-  name: string;
-  href: string;
-  Icon: Icon;
 }
 
 const sidebarOptions: SidebarOption[] = [
@@ -48,7 +42,12 @@ const DashboardLayout = async ({ children }: DashboardLayoutProps) => {
   return (
     <div className="w-full flex h-screen">
       <div className="md:hidden">
-        <MobileChatLayout />
+        <MobileChatLayout
+          friends={friends}
+          session={session}
+          sidebarOptions={sidebarOptions}
+          unseenRequestCount={unseenRequestCount}
+        />
       </div>
       <div className="hidden md:flex h-full w-full max-w-xs grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6">
         <Link href="/dashboard" className="flex h-16 shrink-0 items-center">
